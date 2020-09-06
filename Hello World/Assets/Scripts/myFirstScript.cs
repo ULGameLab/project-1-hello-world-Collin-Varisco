9fbd0abda86e1bf21cbdaca4fe6015716bf7a4d6
Collin-Varisco <70452699+Collin-Varisco@users.noreply.github.com>
2020-09-06 14:44:49 -0500

    First Commit
    Finished Project-1

---
+++ Hello World/Assets/Scripts/myFirstScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class myFirstScript : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        UnityEngine.Debug.Log("Awake function");    
+    }
+
+    void Start()
+    {
+        UnityEngine.Debug.Log("Start function");
+    }
+
+    
+    void Update()
+    {
+        UnityEngine.Debug.Log("Update function");
+    }
+}
